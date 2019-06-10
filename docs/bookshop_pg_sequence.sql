79244f31603dd77f6cff4f9938c8a9e795ff828e
sujini <tgif20146@gmail.com>
2019-06-10 19:51:07 +0900

    2019.06.10
    postgresql enum_book_status 타입 생성

--- docs/bookshop_pg_sequence.sql
+++ docs/bookshop_pg_sequence.sql
@@ -14,3 +14,31 @@ create sequence seq_book start 1;
 select no,name,contents,to_char(reg_date,'yyyy-mm-dd') from guestbook order by reg_date desc;
 
 insert into guestboot values(null,'sujin','hi~','1234',now());
+
+select * from pet;
+
+select * from author;
+
+show tables;
+
+drop sequence seq_author;
+
+create sequence seq_author start 1;
+
+select nextval('seq_author');
+
+insert into author values(nextval('seq_author'),'connan');
+
+delete from author where no = 5;
+
+select no,name,contents,to_char(reg_date,'yyyy-mm-dd') from guestbook order by reg_date desc;
+
+insert into guestbook values(default,'sujin','hi~','1234',now());
+
+select * from member;
+
+create type enum_book_status as enum('대여중','대여가능');
+
+insert into book values(default,'toby spring','',1);
+
+select * from author;
